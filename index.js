7d483757094d42ea56730a099221ba2a1ee8b965
vamshikundella <708vamshi@gmail.com>
2023-07-17 07:13:57 +0000

    super over

---
+++ index.js
@@ -0,0 +1,12 @@
+const element = (
+  //   Write your code here.
+  const element = (
+      <div class="txtContainer">
+        <h1>super Over League</h1>
+        <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"></img>
+        <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png"></img>
+      </div>
+  )
+);
+
+ReactDOM.render(element, document.getElementById("root"));
